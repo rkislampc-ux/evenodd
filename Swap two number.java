3396ab328d49ae6ab92380caa29c97cb15da3d78
rkislampc-ux <rkislampc@gmail.com>
2026-05-03 22:11:05 +0600

    Create Swap two number.java

---
+++ Swap two number.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class SwapNumbers {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter first number: ");
+        int a = sc.nextInt();
+
+        System.out.print("Enter second number: ");
+        int b = sc.nextInt();
+
+        System.out.println("\nBefore Swap: a = " + a + ", b = " + b);
+
+        // Swap without temp variable
+        a = a + b;
+        b = a - b;
+        a = a - b;
+
+        System.out.println("After Swap:  a = " + a + ", b = " + b);
+
+        sc.close();
+    }
+}
