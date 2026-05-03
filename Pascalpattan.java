0b9e9d16a2a1606f16077f85285f2ffc1db5f554
rkislampc-ux <rkislampc@gmail.com>
2026-05-03 22:02:58 +0600

    Create Pascalpattan.java

---
+++ Pascalpattan.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class PascalTriangle {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter number of rows: ");
+        int rows = sc.nextInt();
+
+        for (int i = 0; i < rows; i++) {
+            int number = 1;
+            for (int j = 0; j <= i; j++) {
+                System.out.print(number + " ");
+                number = number * (i - j) / (j + 1); // next number in row
+            }
+            System.out.println();
+        }
+
+        sc.close();
+    }
+}
